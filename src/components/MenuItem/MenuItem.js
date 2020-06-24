01155347996d513e6b7453ed23707f8636ffd5cb
Anshul <anshul.bansal@olx.com>
2020-06-24 19:46:31 +0530

    Addded css

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -19,36 +19,42 @@ const MenuItem = (props) => {
 
   return (
     <div className={css.item}>
-      <div className={css.image}>
-        <img
-          className={"img-fluid"}
-          src={item.item_image_thumb_url || food_image_placeholder}
-          alt="food"
-        />
-      </div>
-      <div className={css.description}>
-        <div className={css.name}>{item.name}</div>
-        <div className={css.buyWrapper}>
-          <span className={css.price}>Rs. {item.price || item.min_price}</span>
-          <span className={css.price}>
-            {quantity === 0 && (
-              <button className="btn" onClick={() => handleAddClick(item)}>
-                Add +
-              </button>
-            )}
-            {!(quantity === 0) && (
-              <div className={css.edit}>
-                <span onClick={() => props.handleItemSelection(item, "remove")}>
-                  <i className="fa fa-minus"></i>
-                </span>
-                <span>{quantity}</span>
-                <span onClick={() => props.handleItemSelection(item)}>
-                  <i className="fa fa-plus"></i>
-                </span>
-              </div>
-            )}
-          </span>
+      <div className={css.info}>
+        {item.item_image_thumb_url && (
+          <div className={css.image}>
+            <img
+              className={"img-fluid"}
+              src={item.item_image_thumb_url || food_image_placeholder}
+              alt="food"
+            />
+          </div>
+        )}
+        <div className={css.description}>
+          <div className={css.name}>{item.name}</div>
+          <div className={css.buyWrapper}>
+            <span className={css.price}>
+              Rs. {item.price || item.min_price}
+            </span>
+          </div>
         </div>
+      </div>
+      <div className={css.quantity}>
+        {quantity === 0 && (
+          <button className="btn" onClick={() => handleAddClick(item)}>
+            Add +
+          </button>
+        )}
+        {!(quantity === 0) && (
+          <div className={css.edit}>
+            <span onClick={() => props.handleItemSelection(item, "remove")}>
+              <i className="fa fa-minus"></i>
+            </span>
+            <span>{quantity}</span>
+            <span onClick={() => props.handleItemSelection(item)}>
+              <i className="fa fa-plus"></i>
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
@@ -64,7 +70,7 @@ const MenuCategory = (props) => {
     <div className={classnames("row", css.category)} id={props.id}>
       {items.map((itemObj, index) => {
         return (
-          <div className="col-md-3" key={index}>
+          <div className="col-12" key={index}>
             <MenuItem
               item={itemObj}
               handleItemSelection={props.handleItemSelection}
